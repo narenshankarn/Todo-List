c68f7dbffe1219ac6dc85df286c61e98b04edb01
Naren N <narenshankarn@gmail.com>
2025-02-04 17:21:11 +0530

    Implemented CRUD operations

--- index.js
+++ index.js
@@ -37,14 +37,39 @@ app.get("/", async(req, res) => {
   }
 });
 
-app.post("/add", (req, res) => {
-  
-  res.redirect("/");
+app.post("/add", async(req, res) => {
+  const item = req.body.newItem;
+  try{
+    db.query("INSERT INTO items(title) VALUES($1)", [item])
+    res.redirect("/");
+  }
+  catch(error) {
+    console.log(error);
+  }
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async (req, res) => {
+  const todo = req.body.updatedItemTitle;
+  const id = req.body.updatedItemId;
+  try{
+    await db.query("UPDATE items SET title = $1 WHERE id = $2", [todo, id]);
+    res.redirect("/");
+  }
+  catch(error) {
+    console.log(error);
+  }
+});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", async (req, res) => {
+  const id = req.body.deleteItemId;
+  try{
+    await db.query("DELETE FROM items WHERE id = $1", [id]);
+    res.redirect("/");
+  }
+  catch(error) {
+    console.log(error);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
